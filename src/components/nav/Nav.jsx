560bd81ac315271cb0b88a2169a3235a7a331840
Jorman Orrala <jorman.orrala.matrix@gmail.com>
2023-06-12 15:31:23 -0500

    feat[navbar]: Update to add navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
+import { BiBook, BiMessageSquareDetail } from 'react-icons/bi'
+import { RiServiceLine } from 'react-icons/ri'
+import './nav.css'
+
 
 const Nav = () => {
     return (
         <nav>
-            
+            <a href="#" className='active'><AiOutlineHome /></a>
+            <a href="#about"><AiOutlineUser /></a>
+            <a href="#experience"><BiBook /></a>
+            <a href="#services"><RiServiceLine /></a>
+            <a href="#contact"><BiMessageSquareDetail /></a>
         </nav>
     )
 }
