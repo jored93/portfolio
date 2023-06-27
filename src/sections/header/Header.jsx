d0ffbdc6580eded3e5beb650a9e630049efe8895
Jorman Orrala <jorman.orrala.matrix@gmail.com>
2023-06-27 10:17:36 -0500

    feat[me]: Update to try profile photo

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import personal from '../../constants/me.json'
+import me from '../../constants/me.json'
 import HeaderSocial from './HeaderSocial'
 
 const Header = () => {
@@ -9,13 +9,13 @@ const Header = () => {
         <header>
             <div className="container header__container">
                 <h3>Hello I'm</h3>
-                <h1>{personal.name}</h1>
-                <h3 className="text-ligth">{personal.profile}</h3>
+                <h1>{me.name}</h1>
+                <h3 className="text-ligth">{me.profile}</h3>
                 <CTA />
                 <HeaderSocial />
 
                 <div className="me">
-                    <img src={personal.image} alt="" />
+                    <img src={me.image} alt="Jored93" />
                 </div>
 
                 <a href="#portfolio" className='scroll__down'>Projects</a>
